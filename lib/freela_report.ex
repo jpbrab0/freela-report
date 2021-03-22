ae2c1d48c1ffb10f673b9a0a9da55aee1232e48d
Joao Pedro <jpresende502@gmail.com>
2021-03-22 18:08:11 -0300

    bug: Fix bug in the map report of report gerenator.

--- lib/freela_report.ex
+++ lib/freela_report.ex
@@ -14,31 +14,16 @@ defmodule FreelaReport do
     "Vinicius"
   ]
 
-  @years [
-    2016,
-    2017,
-    2018,
-    2019,
-    2020
-  ]
-
   def build(filename) do
     filename
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  defp sum_values(
-         [name, hours, _days, months, years],
-         %{
-           "all_hours" => all_hours,
-           "hours_per_month" => hours_per_month,
-           "hours_per_year" => hours_per_year
-         } = report
-       ) do
+  defp sum_values([name, hours, _days, months, years], %{"all_hours" => all_hours, "hours_per_month" => hours_per_month, "hours_per_year" => hours_per_year} = report) do
     all_hours = Map.put(all_hours, name, all_hours[name] + hours)
-    hours_per_month = Map.put(hours_per_month, name, hours_per_month[months] + hours * months)
-    hours_per_year = Map.put(hours_per_year, name, hours_per_year[years] + hours * years)
+    hours_per_month = Map.put(hours_per_month, name, hours_per_month[name] + hours * months)
+    hours_per_year = Map.put(hours_per_year, name, hours_per_year[name] + hours * years)
 
     report
     |> Map.put("all_hours", all_hours)
@@ -48,8 +33,8 @@ defmodule FreelaReport do
 
   defp report_acc do
     all_hours = Enum.into(@freelancers, %{}, &{&1, 0})
-    hours_per_month = Enum.into(1..12, %{}, &{&1, 0})
-    hours_per_year = Enum.into(@years, %{}, &{&1, 0})
+    hours_per_month = Enum.into(@freelancers, %{}, &{&1, 0})
+    hours_per_year = Enum.into(@freelancers, %{}, &{&1, 0})
 
     %{
       "all_hours" => all_hours,
